972bf1e975ec47b835648b7accd33631cb34bcff
Rakesh-99 <rakeshkumarparida424@gmail.com>
2024-05-31 10:16:14 +0530

    complete show blog page ui for admin

--- client/src/components/AllBlogs.jsx
+++ client/src/components/AllBlogs.jsx
@@ -1,85 +1,138 @@
-import { useSelector, useDispatch } from "react-redux";
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-
-
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useSelector } from 'react-redux';
+import { Table, Toast } from 'flowbite-react'
+import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+import toast, { Toaster } from 'react-hot-toast';
 
 
 const AllBlogs = () => {
 
+    const { user } = useSelector((state) => state.userSliceApp);
     const { theme } = useSelector((state) => state.themeSliceApp);
+    const [userBlogs, setUserBlogs] = useState([]);
 
 
 
-    const [blogsInfo, setBlogInfo] = useState(null);
 
-
-    const getAllBlogs = async () => {
-
-        try {
-            const getBlogsInfo = await axios.get('http://localhost:8000/api/blog/getallblogs');
-            const { blogs } = getBlogsInfo.data;
-            setBlogInfo(blogs);
-
-        } catch (error) {
-            console.log(error);
-        }
-    }
 
     useEffect(() => {
-        getAllBlogs();
-    }, [])
+
+        if (user.isAdmin) {
+            const getBlogs = async () => {
+
+                try {
+
+                    const fetchBlogs = await axios.get(`http://localhost:8000/api/blog/getallblogs?${user._id}`);
+
+                    const response = await fetchBlogs.data.blogs;
+                    setUserBlogs(response);
+
+                } catch (error) {
+                    toast.error('An unexpected error occurred!');
+                    console.log(error);
+                }
+            }
+            getBlogs();
+        }
+
+    }, [user._id]);
+
+
+
+    const deleteBlog = async (id) => {
+        console.log(id);
+    }
+
+
 
 
 
 
     return (
-        <div className={`min-h-screen w-full md:mx-5 mx-0 items-center py-5 ${theme === 'dark' ? 'border border-gray-700 shadow-md' : 'border border-gray-200 shadow-md'} `} >
-
-            <div className={`flex justify-between bg-gray-600 py-3 px-4 text-gray-400 rounded-md`}>
-                <div className="flex md:gap-32 gap-10 text-xs md:text-base ">
-                    <span>Date updated</span>
-                    <span>Blog image</span>
-                    <span>Blog title</span>
-                </div>
-                <div className="flex text-xs items-center md:text-base gap-10">
-                    <span>category </span>
-                    <span>Delete</span>
-                    <span>Edit</span>
-                </div>
-            </div>
-
+        < >
             {
-                blogsInfo && blogsInfo.map((data, index) => {
-                    const formattedDate = new Date(data.updatedAt).toLocaleDateString();
-                    return (
-                        <div key={index} className="">
-                            <div className="flex justify-between px-4 py-1 my-4 rounded-md  hover:bg-gray-400 transition-all">
+                user && user.isAdmin && userBlogs.length > 0
+                    ?
+                    <div className='min-h-screen w-full md:mx-10 table-auto overflow-x-scroll scrollbar'>
+                        <Table hoverable className='my-5 '>
 
-                                <div className="flex md:gap-32 items-center gap-10 text-xs md:text-base">
-                                    <p>{formattedDate}</p>
-                                    <div className="md:w-24 w-11">
-                                        <img src={data.blogImgFile} alt="" className="w-full h-12 object-cover rounded-md" />
-                                    </div>
-                                    <p>{data.blogTitle}</p>
-                                </div>
+                            <Table.Head className={` text-base  ${theme === 'dark' ? 'text-gray-100' : 'text-gray-700'} `}>
 
+                                <Table.HeadCell className={`font-semibold text-sm md:text-base border-b ${theme === 'dark' && 'border-gray-500'} px-10 md:px-0`}>Updated on</Table.HeadCell>
 
-                                <div className="flex text-xs md:text-base justify-center  items-center gap-10">
-                                    <p>{data.blogCategory}</p>
-                                    <button className="text-red-500 cursor-pointer">Delete</button>
-                                    <button className="text-green-400 cursor-pointer">Edit</button>
+                                <Table.HeadCell className={`font-semibold text-sm md:text-base border-b ${theme === 'dark' && 'border-gray-500'} px-10 md:px-0`}>Image</Table.HeadCell>
 
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })
+                                <Table.HeadCell className={`font-semibold text-sm md:text-base border-b ${theme === 'dark' && 'border-gray-500'} px-10 md:px-0`}>Title</Table.HeadCell>
+
+                                <Table.HeadCell className={`font-semibold text-sm md:text-base border-b ${theme === 'dark' && 'border-gray-500'} px-10 md:px-0`}>Category</Table.HeadCell>
+
+                                <Table.HeadCell className={`font-semibold text-sm md:text-base border-b ${theme === 'dark' && 'border-gray-500'} px-10 md:px-0`}>
+                                    <span>Edit</span>
+                                </Table.HeadCell>
+
+                                <Table.HeadCell className={`font-semibold text-sm md:text-base border-b ${theme === 'dark' && 'border-gray-500'} px-10 md:px-0`}>Delete</Table.HeadCell>
+                            </Table.Head>
+
+                            {
+                                userBlogs.map((data) => {
+
+                                    const modifiedDate = data.updatedAt.slice(0, 10).toString().split('-').reverse().join('-');
+
+                                    return (
+
+                                        <Table.Body key={data._id}>
+
+                                            <Table.Row key={data._id} className={`text-center text-base md:text-lg  transition-all rounded-md  ${theme === 'dark' ? 'hover:bg-slate-700' : 'hover:bg-slate-200'}`}>
+
+                                                <Table.Cell className='text-sm md:text-base'>
+                                                    {modifiedDate}
+                                                </Table.Cell>
+
+                                                <Table.Cell className='flex justify-center'>
+                                                    <NavLink className='text-center' to={`/blog/${data.slug}`}>
+                                                        <img src={data.blogImgFile} alt="blogImage" className='w-10 text-center rounded-full h-10 md:w-20 md:rounded-md ' />
+                                                    </NavLink>
+                                                </Table.Cell>
+
+                                                <Table.Cell className={`text-sm md:text-base ${theme === 'dark' && 'text-gray-300'}`}>
+                                                    {data.blogTitle}
+                                                </Table.Cell>
+
+                                                <Table.Cell className='text-sm md:text-base'>
+                                                    {data.blogCategory}
+                                                </Table.Cell>
+
+                                                <Table.Cell>
+                                                    <NavLink className='text-green-500 hover:underline' to={`/editblog/${data._id}`}>
+                                                        Edit
+                                                    </NavLink>
+                                                </Table.Cell>
+
+                                                <Table.Cell>
+                                                    <button className='text-red-500 hover:underline' onClick={() => { deleteBlog(data._id) }}>
+                                                        Delete
+                                                    </button>
+                                                </Table.Cell>
+
+                                            </Table.Row>
+
+                                        </Table.Body>
+                                    )
+                                })
+                            }
+
+                        </Table>
+                    </div>
+                    :
+                    <>
+                        <p>No blogs found</p>
+                    </>
             }
-
-        </div >
+            <Toaster />
+        </>
     )
 }
-export default AllBlogs;
+
+export default AllBlogs
