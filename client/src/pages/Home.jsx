9ab6e00c1f2eb0b7d82aaf16b88339b76710aa52
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2024-06-13 01:02:46 +0530

    implement framer motion on some typo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import heroImg from '../assests/HeroImage.png'
-
+import { motion } from 'framer-motion';
 
 
 
@@ -18,12 +18,34 @@ const Home = () => {
           {/* left content  */}
           <div className="flex flex-col gap-5 w-10/12 md:w-1/2">
 
-            <h1 className='text-4xl font-bold text-transparent  bg-clip-text bg-gradient-to-r from-purple-500 to-yellow-300'>Hey Folks, Welcome to my Blog.</h1>
+            <motion.h1 className='text-4xl font-bold text-transparent  bg-clip-text bg-gradient-to-r from-purple-500 to-yellow-300'
+
+              initial={{ x: -100 }}
+              animate={{ x: 10 }}
+              transition={{
+                duration: 1,
+                delay: 0,
+              }}
+            >
+              Hey Folks, Welcome to my Blog.
+            </motion.h1>
+
+
+
             <hr className='' />
 
-            <p className='text-sm text-justify  leading-8'>
+            <motion.p className='text-sm text-justify  leading-8'
+
+              initial={{ x: 200 }}
+              animate={{ x: 10 }}
+              transition={{
+                duration: 1,
+                delay: 0
+              }}
+
+            >
               Discover the latest insights in technology and programming through my compelling blogs. Covering Java, JavaScript, and more, I delve into various topics to keep you informed and engaged. Join and explore the ever-evolving landscape of digital innovation! Here you'll find a variety of articles  on topics such as web development, software engineering, and programming languages
-            </p>
+            </motion.p>
           </div>
 
 
