7407da495d0fde3489c4b7d8588ec9e740fc0a66
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2024-06-28 01:36:44 +0530

    delete route

--- server/routes/commentRoute.js
+++ server/routes/commentRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 const commentRouter = express.Router();
-import { addComment, getUserComments, likeTheComment } from "../controller/commentController.js";
+import { addComment, getUserComments, likeTheComment, deleteComment } from "../controller/commentController.js";
 import verifyUserMiddleware from "../middleware/verifyUserMiddleware.js";
 
 
@@ -12,7 +12,7 @@ commentRouter
     .post('/add-comment/:blogId/:userId', verifyUserMiddleware, addComment)
     .get('/get-comment/:blogId', getUserComments)
     .put('/like-the-comment/:commentId', verifyUserMiddleware, likeTheComment)
-
+    .delete('/delete-comment/:commentId/:userId', deleteComment)
 
 
 
