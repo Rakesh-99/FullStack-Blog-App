f642246d42714920d66a42f30fd909ad381ba13e
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2024-07-01 03:09:54 +0530

    add github call to action and replace the image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,58 @@
 import { useSelector } from 'react-redux';
-import heroImg from '../assests/HeroImage.png'
+import heroImg from '../assests/homeImg.png'
 import { motion } from 'framer-motion';
-
-
-
+import GithubCard from '../components/GithubCard';
+import axios from 'axios'
+import { useEffect } from 'react';
+import RecentBlog from '../components/RecentBlog';
 
 const Home = () => {
 
-  const { blogs } = useSelector((state) => state.blogSliceApp);
+  const { blogs } = useSelector((state) => state.blogSliceApp.blogs);
+
+
+
+
+
+
+  useEffect(() => {
+    const getAllBlogs = async () => {
+      try {
+        const response = await axios.get(`/api/blog/get-all-blogs?limit=9`);
+
+        if (response.status === 200) {
+          console.log(response.data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    getAllBlogs();
+  }, []);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   return (
     <>
-      <div className="min-h-screen">
+      <div className="">
 
         <div className="mt-32 flex justify-around flex-wrap gap-4">
 
@@ -46,6 +87,12 @@ const Home = () => {
             >
               Discover the latest insights in technology and programming through my compelling blogs. Covering Java, JavaScript, and more, I delve into various topics to keep you informed and engaged. Join and explore the ever-evolving landscape of digital innovation! Here you'll find a variety of articles  on topics such as web development, software engineering, and programming languages
             </motion.p>
+
+            <div className="">
+              <GithubCard />
+
+            </div>
+
           </div>
 
 
@@ -54,8 +101,8 @@ const Home = () => {
 
           <div className="w-96  flex-col items-center flex justify-center cursor-not-allowed">
             <img src={heroImg} className='w-full' />
-
           </div>
+
         </div>
       </div>
     </>
